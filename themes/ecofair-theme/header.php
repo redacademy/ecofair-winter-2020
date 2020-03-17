6a8358b6a07cd45e0b3f1dd8cc0ed34ebbdad8ba
DCCA <dcca12@gmail.com>
2020-03-17 11:15:59 -0700

    add logo to the header, remove the mobile version added by the wordpress version

--- themes/ecofair-theme/header.php
+++ themes/ecofair-theme/header.php
@@ -22,12 +22,12 @@
 
 			<header id="masthead" class="site-header" role="banner">
 				<div class="site-branding">
+				<img src="<?php echo get_template_directory_uri() . "/build/assets/logos/GlobeLogo.svg"?>"></img>
 					<h1 class="site-title screen-reader-text"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
-					<p class="site-description"><?php bloginfo( 'description' ); ?></p>
+					<p class="site-description screen-reader-text"><?php bloginfo( 'description' ); ?></p>
 				</div><!-- .site-branding -->
 
 				<nav id="site-navigation" class="main-navigation" role="navigation">
-					<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php echo esc_html( 'Primary Menu' ); ?></button>
 					<?php wp_nav_menu( array( 'theme_location' => 'primary', 'menu_id' => 'primary-menu' ) ); ?>
 				</nav><!-- #site-navigation -->
 			</header><!-- #masthead -->
