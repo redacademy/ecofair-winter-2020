92e9b3798884e0eaf36b474722a3402e03a6480b
hilldmc <hilldmc@gmail.com>
2020-03-17 12:31:47 -0700

    structure of front-page header done

---
+++ themes/ecofair-theme/front-page.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * The template for displaying all pages.
+ *
+ * @package RED_Starter_Theme
+ */
+
+get_header(); ?>
+
+	<div id="primary" class="content-area">
+		<main id="main" class="site-main" role="main">
+
+		<header>
+			<div class="front-banner-grid">
+				
+				<div class="sqr-light-green">
+				</div>
+
+				<div class="sqr-dr-green">
+				</div>
+
+				<div class="sqr-orange">
+				</div>
+
+				<img class="front-banner-img" src="<?php echo get_template_directory_uri(); ?>/build/assets/images/heroimage01.jpeg" alt="a tropical beach" />
+
+			</div> <!-- front-banner-grid -->
+			<h1>Step Up 4 Earth</h1>
+
+		</header>
+
+		</main><!-- #main -->
+	</div><!-- #primary -->
+
+<?php get_sidebar(); ?>
+<?php get_footer(); ?>
