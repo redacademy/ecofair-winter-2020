50d46a6256319a2c1d5fee82992242452d5adbb3
DCCA <dcca12@gmail.com>
2020-03-17 13:34:23 -0700

    add hover effect, add donate button style for last child in the nav

--- themes/ecofair-theme/js/menu.js
+++ themes/ecofair-theme/js/menu.js
@@ -1,10 +1,8 @@
 (function() {
-    console.log("Connected!");
     const menu = document.querySelector(".menu-icon-wrapper");
     const menuList = document.getElementById('primary-menu');
 
     menu.addEventListener('click', (e) => {
 	menuList.classList.toggle('menu-show');
-	console.log("Clicked");
     })
 })();
