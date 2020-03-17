56dbcbaa46f469c3151df0aa622d058b6ba35b83
hilldmc <hilldmc@gmail.com>
2020-03-17 16:03:55 -0700

    added grid for mobile in mixins

--- themes/ecofair-theme/front-page.php
+++ themes/ecofair-theme/front-page.php
@@ -13,14 +13,20 @@ get_header(); ?>
 		<header>
 			<div class="front-banner-grid">
 				
+			<div class="block-container">
 				<div class="sqr-light-green">
 				</div>
+			</div>
 
+			<div class="block-container">
 				<div class="sqr-dr-green">
 				</div>
+			</div>
 
+			<div class="block-container">
 				<div class="sqr-orange">
 				</div>
+			</div>
 				
 				<div class="front-banner-img-box">
 					<img class="front-banner-img" src="<?php echo get_template_directory_uri(); ?>/build/assets/images/heroimage01.jpeg" alt="a tropical beach" />
