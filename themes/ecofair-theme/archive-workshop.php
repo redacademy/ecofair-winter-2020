1e9d9918eb2b5090e5802c625adbcd47682ce2a4
DCCA <dcca12@gmail.com>
2020-03-17 17:56:04 -0700

    add if for loading the title of the workshops archive page just on the desktop version, add the first structure for the first layout for the workshops

--- themes/ecofair-theme/archive-workshop.php
+++ themes/ecofair-theme/archive-workshop.php
@@ -11,19 +11,25 @@ get_header(); ?>
 		<main id="main" class="site-main" role="main">
 
 		<?php if ( have_posts() ) : ?>
-
+			<?php if (!wp_is_mobile()) : ?>
 			<header class="page-header">
-				<?php
-					the_archive_title( '<h1 class="page-title">', '</h1>' );
-				?>
+			    <h1>
+				<?php the_archive_title();?>
+			    </h1>
 			</header><!-- .page-header -->
+			<?php endif; ?>
 
 			<?php /* Start the Loop */ ?>
 			<?php while ( have_posts() ) : the_post(); ?>
+			<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+			    <header class="entry-header">
+			    <?php the_title( sprintf( '<h2 class="entry-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h2>' ); ?>
 
-				<?php
-					get_template_part( 'template-parts/content' );
-				?>
+			    </header><!-- .entry-header -->
+			    <div class="entry-content">
+				<?php the_excerpt(); ?>
+			    </div><!-- .entry-content -->
+			</article><!-- #post-## -->
 
 			<?php endwhile; ?>
 
