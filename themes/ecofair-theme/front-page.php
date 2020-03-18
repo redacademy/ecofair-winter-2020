106ae7570856a188947153488f4f854c9e5d7833
hilldmc <hilldmc@gmail.com>
2020-03-18 10:28:49 -0700

    add custom fields for event details on front page

--- themes/ecofair-theme/front-page.php
+++ themes/ecofair-theme/front-page.php
@@ -16,21 +16,20 @@ get_header(); ?>
 				<div class="sqr-light-green">
 				</div>
 
-		
 				<div class="sqr-dr-green">
 				</div>
-
 
 				<div class="sqr-orange">
 				</div>
 
 				<img class="front-banner-img" src="<?php echo get_template_directory_uri(); ?>/build/assets/images/heroimage01.jpeg" alt="a tropical beach" />
-
 				
 			</div> <!-- front-banner-grid -->
 
-
 			<h1>Step Up 4 Earth</h1>
+			<h2 class="event-title"><?php the_field('name_of_event'); ?></h2>
+			<p class="location"><?php the_field('location_of_event'); ?></p>
+			<p class="date-time"><?php the_field('day_of_event');?>, <?php the_field('time_of_event'); ?></p>
 
 		</header>
 
