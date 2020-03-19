34fa06a5eefbb2f15c9f23373aa69730fab59020
Daniel <dcca12@gmail.com>
2020-03-19 12:01:25 -0700

    add flickity start script, add buttons for previous and next working

---
+++ themes/ecofair-theme/js/start-carousel.js
@@ -0,0 +1,18 @@
+(function() {
+    const carousel = document.querySelector('.carousel');
+    const flkty = new Flickity(carousel, {
+	hash: true
+    });
+    const buttonPrev = document.querySelectorAll('.carousel-pagination-prev');
+    buttonPrev.forEach( button => {
+	button.addEventListener('click', () => {
+	    flkty.previous();
+	})
+    } )
+    const buttonNext = document.querySelectorAll('.carousel-pagination-next');
+    buttonNext.forEach( button => {
+	button.addEventListener('click', () => {
+	    flkty.next();
+	})
+    })
+})();
