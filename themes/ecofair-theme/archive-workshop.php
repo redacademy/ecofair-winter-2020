ef98c9b9e7c63b0f9c741fd834fa6287b2d71e56
Daniel <dcca12@gmail.com>
2020-03-19 17:58:04 -0700

    fix the variable sharing problem with templates in the workshop archive page

--- themes/ecofair-theme/archive-workshop.php
+++ themes/ecofair-theme/archive-workshop.php
@@ -69,46 +69,8 @@ get_header(); ?>
 			    $counter = $counter + 1;
 			?>
 			<!-- Start the carousel cell -->
-			<div class="carousel-cell" id="<?php echo 'carousel-cell' . $counter; ?>">
-			<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-			<div class="workshop-order-number">
-			    <p><?php echo $counter ;?></p>
-			</div>
-			<div class="workshop-title-image" style="background-image: url(<?php echo get_field("title_image") ?>)">
-			</div>
-			    <header class="entry-header">
-				<?php the_title( sprintf( '<h2 class="entry-title">', esc_url( get_permalink() ) ), '</h2>' ); ?>
-			    </header><!-- .entry-header -->
-			    <div class="workshop-owner">
-				<h3><?php echo get_field("owner"); ?></h3>
-			    </div>
-			<div class="workshop-secondary-image" style="background-image: url(<?php echo get_field("secondary_image") ?>)">
-			</div>
-			<!-- If is not mobile, hide pagination -->
-			<?php if ( wp_is_mobile() ) : ?>
-			<div class="carousel-pagination">
-			    <div class="carousel-pagination-numbers"></div>
-			    <div class="carousel-pagination-buttons">
-				<div class="carousel-pagination-prev">
-				    <i class="fas fa-arrow-left"></i>
-				</div>
-				<div class="carousel-pagination-next">
-				    <i class="fas fa-arrow-right"></i>
-				</div>
-			    </div>
-			</div>
-			<?php endif; ?>
-			<div class="entry-content">
-			    <?php the_excerpt(); ?>
-			    <a class="volunteer-button" href="#">Volunteer</a>
-			</div><!-- .entry-content -->
-			<div class="light-green-box"></div>
-			<div class="dark-green-box"></div>
-			<div class="orange-green-box"></div>
-			<div class="green-line"></div>
-			</article><!-- #post-## -->
-			</div>
-			<?php array_push($workshop_carousel_links, '#carousel-cell'.$counter); ?>
+				<?php include( locate_template( 'template-parts/content-workshopnoowner.php', false) ); ?>
+			<!-- End ForEach -->
 			<?php endforeach ; ?>
 			</div>
 			<!-- Create the links for the carousel -->
@@ -116,8 +78,9 @@ get_header(); ?>
 			<?php 
 			    foreach($workshop_carousel_links as $carousel_id) :
 			?>
-			<a href="<?php echo $carousel_id; ?>"><?php echo substr($carousel_id, -1); ?></a>
+			    <a href="<?php echo $carousel_id; ?>"><?php echo substr($carousel_id, -1); ?></a>
 			<?php endforeach; ?>
+			<!-- Run script to add the navigation for specific DIVs -->
 			<script type="text/javascript">
 				const carouselLinks = document.querySelector(".carousel-generator").innerHTML;
 				const carouselDiv = document.querySelectorAll(".carousel-pagination-numbers");
@@ -127,9 +90,6 @@ get_header(); ?>
 			</script>
 			</div>
     
-
-			<?php the_posts_navigation(); ?>
-
 		<?php else : ?>
 
 			<?php get_template_part( 'template-parts/content', 'none' ); ?>
