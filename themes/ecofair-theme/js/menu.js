6d52e6935ce0a59b1dbe5350a28ce3a17bd11445
Daniel <dcca12@gmail.com>
2020-03-21 10:52:21 -0700

    start addind border bottom to selected id on all carousel

--- themes/ecofair-theme/js/menu.js
+++ themes/ecofair-theme/js/menu.js
@@ -1,8 +1,8 @@
 (function() {
-    const menu = document.querySelector(".menu-icon-wrapper");
+    const menu = document.querySelector('.menu-icon-wrapper');
     const menuList = document.getElementById('primary-menu');
 
-    menu.addEventListener('click', (e) => {
+    menu.addEventListener('click', () => {
 	menuList.classList.toggle('menu-show');
     })
 })();
--- themes/ecofair-theme/js/start-carousel.js
+++ themes/ecofair-theme/js/start-carousel.js
@@ -1,5 +1,6 @@
 (function() {
     const carousel = document.querySelector('.carousel');
+    const numberPag = document.querySelector('.carousel-pagination-numbers').childNodes;
     const flkty = new Flickity(carousel, {
 	hash: true,
 	draggable: false
@@ -8,8 +9,9 @@
     buttonPrev.forEach( button => {
 	button.addEventListener('click', () => {
 	    flkty.previous();
+	    console.log(numberPag);
 	})
-    } )
+    })
     const buttonNext = document.querySelectorAll('.carousel-pagination-next');
     buttonNext.forEach( button => {
 	button.addEventListener('click', () => {
