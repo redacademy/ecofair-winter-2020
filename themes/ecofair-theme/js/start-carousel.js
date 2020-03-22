3c18e72ed44ccaa33ec2d783c1c7f2d269a06fd0
Daniel <dcca12@gmail.com>
2020-03-22 10:53:05 -0700

    add script to keep track of selected slide in the carousel

--- themes/ecofair-theme/js/start-carousel.js
+++ themes/ecofair-theme/js/start-carousel.js
@@ -1,21 +1,33 @@
 (function () {
     const carousel = document.querySelector('.carousel');
+    const carouselNum = document.querySelectorAll('.carousel-pagination-numbers>a');
     const flkty = new Flickity(carousel, {
         hash: true,
-        cellAlign: "left",
+        cellAlign: 'left',
         draggable: false
     });
+    function getAttr(arr) {
+	const number = (window.location.href).slice(-1);
+	arr.forEach(e => {
+	    if(e.getAttribute('href').slice(-1) === number){
+		e.classList.add('active-number');
+	    }else {
+		e.classList.remove('active-number');
+	    }
+	});
+    }
     const buttonPrev = document.querySelectorAll('.carousel-pagination-prev');
     buttonPrev.forEach(button => {
-        button.addEventListener('click', () => {
-            flkty.previous();
-        })
+	button.addEventListener('click', () => {
+	    flkty.previous();
+	    getAttr(carouselNum);
+	})
     })
     const buttonNext = document.querySelectorAll('.carousel-pagination-next');
     buttonNext.forEach(button => {
-        button.addEventListener('click', () => {
-            flkty.next();
-        })
+	button.addEventListener('click', () => {
+	    flkty.next();
+	    getAttr(carouselNum);
+	})
     })
-
 })();
