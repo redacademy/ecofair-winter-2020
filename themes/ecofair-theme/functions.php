0b3316da2adaad11c3450fc9da3b1de498b423ce
hilldmc <hilldmc@gmail.com>
2020-03-22 16:59:30 -0700

    adds banner sidebar to add social media widget to front banner

--- themes/ecofair-theme/functions.php
+++ themes/ecofair-theme/functions.php
@@ -83,6 +83,16 @@ function ecofair_widgets_init() {
 		'before_title'  => '<h2 class="features-widget-title">',
 		'after_title'   => '</h2>',
 	) );
+
+	register_sidebar( array(
+		'name'          => esc_html( 'Banner' ),
+		'id'            => 'banner-widgets',
+		'description'   => '',
+		'before_widget' => '<aside id="%1$s" class="widget %2$s">',
+		'after_widget'  => '</aside>',
+		'before_title'  => '<h2 class="banner-widget-title">',
+		'after_title'   => '</h2>',
+	) );
 }
 add_action( 'widgets_init', 'ecofair_widgets_init' );
 
