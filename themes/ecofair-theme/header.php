a5380f7d1a3d6686a6472a3d5c7c101094017578
Daniel <dcca12@gmail.com>
2020-03-23 11:26:40 -0700

    fix the donate button in the header, fix when the user click outside the header it closes now, fix the hover in desktop header

--- themes/ecofair-theme/header.php
+++ themes/ecofair-theme/header.php
@@ -28,7 +28,7 @@
 					<h1 class="site-title screen-reader-text"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
 					<p class="site-description screen-reader-text"><?php bloginfo( 'description' ); ?></p>
 				<div class="menu-icon-wrapper">
-				<i class="fas fa-bars"></i>
+				    <i class="fas fa-bars"></i>
 				</div>
 				</div><!-- .site-branding -->
 
--- themes/ecofair-theme/js/menu.js
+++ themes/ecofair-theme/js/menu.js
@@ -1,8 +1,15 @@
 (function() {
     const menu = document.querySelector('.menu-icon-wrapper');
     const menuList = document.getElementById('primary-menu');
+    const header = document.getElementById('masthead');
 
     menu.addEventListener('click', () => {
 	menuList.classList.toggle('menu-show');
+    })
+    document.addEventListener('click', e => {
+	const menuElem = header.contains(e.target);
+	if(!menuElem){
+	    menuList.classList.remove('menu-show');
+	}
     })
 })();
