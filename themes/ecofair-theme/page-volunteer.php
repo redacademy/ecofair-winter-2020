f6fade31b916f96ce03f39a43589a271a9a34f08
Daniel <dcca12@gmail.com>
2020-03-24 11:09:54 -0700

    add basic structure to the volunteer page

--- themes/ecofair-theme/page-volunteer.php
+++ themes/ecofair-theme/page-volunteer.php
@@ -11,9 +11,21 @@ get_header(); ?>
 		<main id="main" class="site-main" role="main">
 
 			<?php while ( have_posts() ) : the_post(); ?>
-<h1>Teste</h1>
-				<?php get_template_part( 'template-parts/content', 'page' ); ?>
+			    <article id="post-<?php the_ID(); ?>" <?php post_class('volunteer-content'); ?>>
+				    <header class="entry-header">
+					    <?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
+				    </header><!-- .entry-header -->
 
+				    <div class="entry-content">
+					    <?php the_content(); ?>
+					    <?php
+						    wp_link_pages( array(
+							    'before' => '<div class="page-links">' . esc_html( 'Pages:' ),
+							    'after'  => '</div>',
+						    ) );
+					    ?>
+				    </div><!-- .entry-content -->
+			    </article><!-- #post-## -->
 			<?php endwhile; // End of the loop. ?>
 
 		</main><!-- #main -->
