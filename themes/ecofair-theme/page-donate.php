52202fcb2567012a017ac1838289c9bf004d623b
Daniel <dcca12@gmail.com>
2020-03-25 16:07:45 -0700

    create the page-donate.php

---
+++ themes/ecofair-theme/page-donate.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * The template for displaying volunteer page.
+ *
+ * @package RED_Starter_Theme
+ */
+
+get_header(); ?>
+	<div class="background-grey-box"></div>
+	<div class="background-green-box"></div>
+	<div id="primary" class="content-area">
+		<main id="main" class="site-main" role="main">
+
+			<?php while ( have_posts() ) : the_post(); ?>
+			    <article id="post-<?php the_ID(); ?>" <?php post_class('volunteer-content'); ?>>
+				    <header class="entry-header">
+					    <?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
+				    </header><!-- .entry-header -->
+
+				    <div class="entry-content">
+					    <?php the_content(); ?>
+					    <?php
+						    wp_link_pages( array(
+							    'before' => '<div class="page-links">' . esc_html( 'Pages:' ),
+							    'after'  => '</div>',
+						    ) );
+					    ?>
+				    </div><!-- .entry-content -->
+			    </article><!-- #post-## -->
+			<?php endwhile; // End of the loop. ?>
+
+		</main><!-- #main -->
+	</div><!-- #primary -->
+
+
+<?php get_footer(); ?>
