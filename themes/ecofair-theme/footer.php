1e69d86257333cd169d9c0e1cd158885058cca69
hilldmc <hilldmc@gmail.com>
2020-03-25 12:25:21 -0700

    refractor code to use reponsive measurements when applicable

--- themes/ecofair-theme/footer.php
+++ themes/ecofair-theme/footer.php
@@ -10,14 +10,17 @@
 			</div><!-- #content -->
 
 			<footer id="colophon" class="site-footer" role="contentinfo">
+				
 				<div class="footer-container">
 					<?php wp_nav_menu();?>
 				  <div class="widget-area">
 					<?php dynamic_sidebar('footer_sidebar'); ?>
 				  </div><!-- .widget-area -->
 				</div><!-- .footer-container -->
+
 				<p class="rights-reserved" >© 2020 STEP UP 4 EARTH, ALL RIGHTS RESERVED.</p>
-			</footer><!-- #colophon -->
+			</footer><!-- .site-footer -->
+
 		</div><!-- #page -->
 
 		<?php wp_footer(); ?>
