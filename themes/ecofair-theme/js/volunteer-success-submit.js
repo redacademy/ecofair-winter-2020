5fe355e52ae0a0d96be8caa72695994bd4bd5816
Daniel <dcca12@gmail.com>
2020-03-27 08:46:59 -0700

    remove comments in the redirect js

--- themes/ecofair-theme/js/volunteer-success-submit.js
+++ themes/ecofair-theme/js/volunteer-success-submit.js
@@ -1,5 +1,4 @@
 (function() {
-    //const submitBtn = document.querySelector('.wpcf7-submit');
     const url = homeUrl.url;
     const wpcf7Elm = document.querySelector( '.wpcf7' );
     wpcf7Elm.addEventListener('wpcf7mailsent', () => {
