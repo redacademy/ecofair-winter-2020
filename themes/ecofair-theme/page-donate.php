4a63d999bdbaef0ad29d3302a843087652c0116c
hilldmc <hilldmc@gmail.com>
2020-04-03 10:11:44 -0700

    replaces custom button with paypal one because of timeout

--- themes/ecofair-theme/page-donate.php
+++ themes/ecofair-theme/page-donate.php
@@ -24,7 +24,13 @@ get_header(); ?>
 							    'before' => '<div class="page-links">' . esc_html( 'Pages:' ),
 							    'after'  => '</div>',
 						    ) );?>
-					    <a class="donate-button" href="<?php echo get_field('donation_link'); ?>" target=”_blank”>Donate</a>
+					    <!-- <a class="donate-button" href="<?php echo get_field('donation_link'); ?>" target=”_blank”>Donate</a> -->
+							<form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">
+<input type="hidden" name="cmd" value="_s-xclick">
+<input type="hidden" name="hosted_button_id" value="USXZ3X7P6K89W">
+<input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_donateCC_LG.gif"  name="submit" title="PayPal - The safer, easier way to pay online!" alt="Donate with PayPal button">
+<img alt=""  src="https://www.paypal.com/en_CA/i/scr/pixel.gif" width="1" height="1">
+</form>
 				    </div><!-- .entry-content -->
 			    </article><!-- #post-## -->
 			<?php endwhile; // End of the loop. ?>
